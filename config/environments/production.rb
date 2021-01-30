2a8a684b3f22e4b07a8c3da30043327be4daf4a8
Harritone <yourrasik@gmail.com>
2021-01-31 00:01:10 +1000

    Add: url to production"

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,7 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  config.action_mailer.default_url_options = { host: 'https://boiling-tundra-66334.herokuapp.com/' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
