3500c3a27fcf6b57d4a85ec804927ea1b768d06e
Harritone <yourrasik@gmail.com>
2021-01-31 16:23:36 +1000

    Add: style for courses and landing page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,9 @@
 class StaticPagesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:landing_page]
   def landing_page
+    @courses_pop = Course.limit(3)
+    @courses_top = Course.limit(3)
+    @courses_latest = Course.all.order(created_at: :desc).limit(3)
   end
 
   def privacy_policy
