cffb31f28a65b908af024c2f550a0de3387c700c
Harritone <yourrasik@gmail.com>
2021-01-31 18:34:51 +1000

    Add: gem friendly_id

--- Gemfile
+++ Gemfile
@@ -32,6 +32,7 @@ gem 'haml-rails', '~> 2.0', '>= 2.0.1'
 gem 'simple_form'
 gem 'faker'
 gem 'devise'
+gem 'friendly_id', '~> 5.4', '>= 5.4.2'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,13 @@
 class Course < ApplicationRecord
+  extend FriendlyId
+  friendly_id :title, use: :slugged
+
+  # friendly_id :generated_slug, use: :slugged
+  # def generated_slug
+  #   require 'securerandom'
+  #   @random_slug ||= persisted? ? friendly_id : SecureRandom.hex(4)
+  # end
+
   validates :title, presence: true, length: {minimum: 5 }
   validates :description, presence: :true, length: {minimum:5} 
 
