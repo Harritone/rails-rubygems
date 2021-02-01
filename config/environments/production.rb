12ece0761c7cb2642d04b9b6066775a33cdfe80c
Harritone <yourrasik@gmail.com>
2021-02-01 20:31:58 +1000

    Try: without ::Rails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,7 +16,7 @@ Rails.application.configure do
   #   enable_starttls_auto: true
   # }
   aws_credentials = Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_ACCESS_KEY_ID'])
-  Aws::Rails.add_action_mailer_delivery_method(:aws_ses, credentials: aws_credentials, region: ENV['AWS_REGION'])
+  Aws.add_action_mailer_delivery_method(:aws_ses, credentials: aws_credentials, region: ENV['AWS_REGION'])
 
   config.action_mailer.delivery_method = :aws_ses
   # Settings specified here will take precedence over those in config/application.rb.
