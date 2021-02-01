34fbcc143a623cb22308bfc00460fb59c02eb9c7
Harritone <yourrasik@gmail.com>
2021-02-01 20:38:52 +1000

    Fix: Aws::Rails

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,3 +1,3 @@
   require 'json'
   aws_credentials = Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_ACCESS_KEY_ID'])
-  Aws.add_action_mailer_delivery_method(:aws_ses, credentials: aws_credentials, region: ENV['AWS_REGION'])
+  Aws::Rails.add_action_mailer_delivery_method(:aws_ses, credentials: aws_credentials, region: ENV['AWS_REGION'])
