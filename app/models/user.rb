523d6888e8e9b76db3484bf85a7513a9b44a12c5
Harritone <yourrasik@gmail.com>
2021-02-04 22:30:45 +1000

    Add: roles to users on save

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,21 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :confirmable
   
   rolify
+
   has_many :courses
+
+  after_create :assign_default_role
+
+  def assign_default_role
+    if User.count == 1
+      self.add_role(:admin) if self.roles.blank?
+      self.add_role(:teacher) 
+      self.add_role(:student) 
+    else
+      self.add_role(:student) if self.roles.blank?
+      self.add_role(:teacher) 
+    end
+  end
 
   def username
     self.email.split(/@/).first
