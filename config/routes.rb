4c2dd54816062519794626f55b246e72f4209d6c
Harritone <yourrasik@gmail.com>
2021-02-06 18:58:02 +1000

    Add: nest lessons inside courses

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  resources :lessons
+  resources :courses do
+    resources :lessons
+  end
   devise_for :users
   resources :users, only: [:index, :edit, :show, :update]
-  resources :courses
   root 'static_pages#landing_page'
   get 'privacy_policy', to: 'static_pages#privacy_policy'
   get 'activity', to: 'static_pages#activity'
