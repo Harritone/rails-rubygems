a5f3096146335ba117dcded727160492ff1d48ff
Harritone <yourrasik@gmail.com>
2021-02-06 13:20:56 +1000

    Add: action_text for lessons, breadcrumbs

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -8,6 +8,8 @@ class Lesson < ApplicationRecord
   include PublicActivity::Model
   tracked owner: Proc.new {|controller, model| controller.current_user}
 
+  has_rich_text :content
+
   def to_s
     title
   end
