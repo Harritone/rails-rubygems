a0498d489b1ebd386e398437632f72f8bdcf49e1
Harritone <yourrasik@gmail.com>
2021-02-06 17:16:24 +1000

    Add: authorzation lessons

---
+++ app/policies/lesson_policy.rb
@@ -0,0 +1,24 @@
+class LessonPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def edit?
+    @record.course.user_id == @user.id
+  end
+
+  def update?
+    @record.course.user_id == @user.id
+  end
+
+  def show?
+    @record.course.user_id == @user.id || @user.has_role?(:admin)
+  end
+
+  def destroy?
+    @record.course.user_id == @user.id
+  end
+
+end
